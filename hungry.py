5356824611d5ef56c785aad9c7c42c84db3cef17
Somesh Deshmukh <somesh.deshmukh@blenheimchalcot.com>
2023-09-18 14:37:09 +0530

    addition, substraction, mul and div fucntion addded

--- hungry.py
+++ hungry.py
@@ -6,3 +6,15 @@ def address():
     print("This is 2nd new thing added")
     print("This is new thing added")
 
+def addition():
+    print(3+3)
+
+def substraction():
+    print(3-2)
+
+def mul():
+    print(3*3)
+
+def div():
+    print(10/2)
+    
