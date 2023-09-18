4b95fbb317f3254cc4f1d37d2575dab6d5aa4fac
Somesh Deshmukh <somesh.deshmukh@blenheimchalcot.com>
2023-09-18 13:41:55 +0530

    message

--- hungry.py
+++ hungry.py
@@ -1,17 +1,3 @@
-hungry = input("Are you hungry")
-if hungry == "yes":
-    print("eat samosa")
-    print("drink badamshake")
-    print("drink kajushake")
-    
-else:
-    thirsty = input("are you thirsty")
-    if thirsty == "yes":
-        print("drink water")
-        print("drink soda")
-    print("do your homework")
-
-
 
 
 
