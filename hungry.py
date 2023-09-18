e386ccbf3e8005fc53a77e39c21f9b20ca68f4e9
Somesh Deshmukh <somesh.deshmukh@blenheimchalcot.com>
2023-09-18 14:59:06 +0530

    some notes added

--- hungry.py
+++ hungry.py
@@ -17,4 +17,21 @@ def mul():
 
 def div():
     print(10/2)
-    
+
+# Certainly! Here's a concise and easy-to-note explanation of the command git push --set-upstream origin address:
+
+# git push: This command is used to send your local code changes to a remote repository.
+
+# --set-upstream (or -u for short): This option tells Git to remember where to push your code in the future without you needing to specify it each time.
+
+# origin: It's the name of the remote repository, usually named "origin" by default, where your code is stored remotely.
+
+# address: This is the name of your branch both locally and remotely.
+
+# So, when you run git push --set-upstream origin address, you're telling Git to:
+
+# Push your local "address" branch to the "address" branch on the remote repository named "origin."
+
+# Remember this relationship, so in the future, you can simply use git push and git pull without specifying the remote and branch names, making your workflow more efficient.
+
+# This is especially useful when working with branches that aren't yet on the remote or when collaborating with others on new features or fixes.
